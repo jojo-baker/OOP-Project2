3f7f87c52ecc582d09e50216d16646b9b084e6dd
“jojo-baker” <jo.alice.baker@gmail.com>
2019-08-30 17:50:46 +0800

    Everything is connected, just need the detail

--- Survey.py
+++ Survey.py
@@ -10,6 +10,9 @@ class Survey:
     def start_survey(self):
         pass
 
+# Welcome message
+print('\nWelcome to my survey!\nPlease answer honestly.')
+
 # Here are the first three qualifying questions
 question_prompts = [
     '\n----------Q1----------\nSomething about Wednesday\'s class\n(a) Yes\n(b) No\n\n',
@@ -38,26 +41,73 @@ section1_questions = [
 ]
 
 
-# Welcome message
-print('\nWelcome to my survey!\nPlease answer honestly.')
 
-# section2 = Section('\n\n*****************************\n       Section 2 Title\n*****************************', '\nSection 2 Description')
-# section3 = Section('\n\n*****************************\n       Section 3 Title\n*****************************', '\nSection 3 Description')
+# Here is the second section - title, description and questions
+# Section 2 title and descripion
+section2 = Section('\n\n*****************************\n       Section 2 Title\n*****************************', '\nSection 2 Description')
+# Section 2 questions
+section2_question_prompts = [
+   '\n----------SQ1----------\nSecond section question\n(a) answer 1\n(b) answer 2\n(c) answer 3\n\n',
+   '\n----------SQ2----------\nSecond section question\n(a) answer 1\n(b) answer 2\n(c) answer 3\n\n',
+   '\n----------SQ3----------\nSecond section question\n(a) answer 1\n(b) answer 2\n(c) answer 3\n\n',
+]
+section2_questions = [
+    Question(section2_question_prompts[0], ' '),
+    Question(section2_question_prompts[1], ' '),
+    Question(section2_question_prompts[2], ' '),
+]
 
-def run_section1():
+# Here is the third section - title, description and questions
+# Section 3 title and descripion
+section3 = Section('\n\n*****************************\n       Section 3 Title\n*****************************', '\nSection 3 Description')
+# Section 3 questions
+section3_question_prompts = [
+   '\n----------SQ1----------\nThird section question\n(a) answer 1\n(b) answer 2\n(c) answer 3\n\n',
+   '\n----------SQ2----------\nThird section question\n(a) answer 1\n(b) answer 2\n(c) answer 3\n\n',
+   '\n----------SQ3----------\nThird section question\n(a) answer 1\n(b) answer 2\n(c) answer 3\n\n',
+]
+section3_questions = [
+    Question(section3_question_prompts[0], ' '),
+    Question(section3_question_prompts[1], ' '),
+    Question(section3_question_prompts[2], ' '),
+]
+
+# Section 1 Function
+def run_section1(section1_questions):
     print(section1.section_title)
     print(section1.description)
-    def run_section1_questions(section1_questions):
-        for question in section1_questions:
-            while True:
-                answer = input(question.prompt)
-                if answer.lower() in ['a', 'b', 'c']:
-                    print('***** Great, you answered ' + answer)
-                    break
-                else:
-                    print('***** Nope, not valid. Try that again. I\'m only accepting a, b or c.')
-    run_section1_questions(section1_questions)
-
+    for question in section1_questions:
+        while True:
+            answer = input(question.prompt)
+            if answer.lower() in ['a', 'b', 'c']:
+                print('***** Great, you answered ' + answer)
+                break
+            else:
+                print('***** Nope, not valid. Try that again. I\'m only accepting a, b or c.')
+# Section 2 Function
+def run_section2(section2_questions):
+    print(section2.section_title)
+    print(section2.description)
+    for question in section2_questions:
+        while True:
+            answer = input(question.prompt)
+            if answer.lower() in ['a', 'b', 'c']:
+                print('***** Great, you answered ' + answer)
+                break
+            else:
+                print('***** Nope, not valid. Try that again. I\'m only accepting a, b or c.')
+# Section 3 Function
+def run_section3(section3_questions):
+    print(section3.section_title)
+    print(section3.description)
+    for question in section3_questions:
+        while True:
+            answer = input(question.prompt)
+            if answer.lower() in ['a', 'b', 'c']:
+                print('***** Great, you answered ' + answer)
+                break
+            else:
+                print('***** Nope, not valid. Try that again. I\'m only accepting a, b or c.')
 
 
 #this is the start of the survey qualifying questions, but it isn't executed until the bottom
@@ -78,12 +128,11 @@ def run_survey(qualifying_questions):
             else:
                 print('***** Hmmmmmmm, that\'s not quite right...Please try again.')
     if sections_to_show[0] == 'a':
-        run_section1()
-
+        run_section1(section1_questions)
     if sections_to_show[1] == 'a':
-        print('this is the second section')
+        run_section2(section2_questions)
     if sections_to_show[2] == 'a':
-        print('this is the third section')
+        run_section3(section3_questions)
 
 # This starts the survey
 run_survey(qualifying_questions)
